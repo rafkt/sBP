b9c5d54d41000eecbb5cda66e511b807d91b0955
Rafael Ktistakis <rafaelktistakis@gmail.com>
2018-11-19 12:50:41 +0000

    Seting up and experiment to measure rank/select on wt_int<>; This will help to determine how much time I spend in backwardsearch predictor by counting how many rank selects are performed in different parts of the algorithm

---
+++ simple_bs_predictor/rank_select_performance_test.cpp
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <fstream>
+#include "suffixArray.h"
+#include <stack>
+#include <algorithm>
+
+using namespace suffixArray;
+
+
+int main(int argc, const char * argv[])
+{
+
+
+// Main idea is to load a fold and contract BWT wt_int<> L & alphabet.
+// For a 100 alphabet random items do rank (what about the position ? random or contant ? think..)
+// Clock all the ranks together. Output the result
+// Do the same for select (position ?)
+// Repeat for all folds/datasets using parallel -> get an average time for 100ranks and 100selects
+
+
+}
