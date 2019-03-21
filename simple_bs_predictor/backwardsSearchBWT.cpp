6268ea43b151f3c8a2c31256b6e2cfdfd3b27b48
Rafael Ktistakis <rafaelktistakis@gmail.com>
2019-03-21 12:11:00 +0000

    Re-ran experiments where I corrected the typos - exports are now replaced with the correct ones

--- simple_bs_predictor/backwardsSearchBWT.cpp
+++ simple_bs_predictor/backwardsSearchBWT.cpp
@@ -187,7 +187,7 @@ int backwardsSearchBWT::search(int c, int rangeStart, int rangeEnd, int& newRang
         rankCounter += 2;
         int rankStart = L.rank(rangeStart, c);
         int rankEnd = L.rank(rangeEnd + 1, c);
-        cout << c << " " << rankStart << " " << rangeEnd << endl;
+        cout << c << " " << rangeStart << " " << rangeEnd << endl;
         if (rankEnd - rankStart == 0) return -1; //patern not found
         // int letterPos = alphabet.select(1, c);
         // if (letterPos != c) cout << "ERROR2 : " << letterPos << " : " << c << endl;
