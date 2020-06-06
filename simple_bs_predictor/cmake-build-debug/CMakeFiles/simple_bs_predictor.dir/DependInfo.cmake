0fd4baf51a44229036a634b60e110557b4347b6d
Rafael Ktistakis <rafaelktistakis@gmail.com>
2020-06-07 00:39:15 +0100

    CMake compatible project and added conversion of the Makefile to export from compileDb
    In total:
    - CompileDb export (compile_database) will be used for compilation/intelisense
    - Makefile will be also used for build
    - CMake project to add support for including the external sdsl library

---
+++ simple_bs_predictor/cmake-build-debug/CMakeFiles/simple_bs_predictor.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/backwardsSearchBWT.cpp" "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/cmake-build-debug/CMakeFiles/simple_bs_predictor.dir/backwardsSearchBWT.cpp.o"
+  "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/main.cpp" "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/cmake-build-debug/CMakeFiles/simple_bs_predictor.dir/main.cpp.o"
+  "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/predictor.cpp" "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/cmake-build-debug/CMakeFiles/simple_bs_predictor.dir/predictor.cpp.o"
+  "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/subseqPredictor.cpp" "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/cmake-build-debug/CMakeFiles/simple_bs_predictor.dir/subseqPredictor.cpp.o"
+  "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/suffixArray.cpp" "/Users/rafaelktistakis/Repositories/sBP/simple_bs_predictor/cmake-build-debug/CMakeFiles/simple_bs_predictor.dir/suffixArray.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "/Users/rafaelktistakis/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simple_bs_predictor/cmake-build-debug/CMakeFiles/simple_bs_predictor.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_bs_predictor.dir/backwardsSearchBWT.cpp.o"
+  "CMakeFiles/simple_bs_predictor.dir/main.cpp.o"
+  "CMakeFiles/simple_bs_predictor.dir/predictor.cpp.o"
+  "CMakeFiles/simple_bs_predictor.dir/subseqPredictor.cpp.o"
+  "CMakeFiles/simple_bs_predictor.dir/suffixArray.cpp.o"
+  "simple_bs_predictor"
+  "simple_bs_predictor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_bs_predictor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
